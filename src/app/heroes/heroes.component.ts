037b2d6d74808229e33e005df9c3018647fd31ab
hungdao <hung.dao1@tiki.vn>
2022-01-28 22:44:26 +0700

    add navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {Hero, mockHeroes} from "../../models/hero";
+import {Component, OnInit} from '@angular/core';
+import {mockHeroes} from "../../models/hero";
 import {HeroService} from "../../service/hero/hero.service";
-import {MessageService} from "../../service/message/message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -11,20 +10,12 @@ import {MessageService} from "../../service/message/message.service";
 export class HeroesComponent implements OnInit {
   heroes = mockHeroes;
   constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
+    private heroService: HeroService
   ) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
-
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id = ${hero.id}`)
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
--- src/service/hero/hero.service.ts
+++ src/service/hero/hero.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Hero, mockHeroes} from "../../models/hero";
 import {MessageService} from "../message/message.service";
 import {Observable, of} from "rxjs";
@@ -14,5 +14,13 @@ export class HeroService {
     const heroes = of(mockHeroes);
     this.messageService.add('hero service: fetched heroes');
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = mockHeroes.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
 }
